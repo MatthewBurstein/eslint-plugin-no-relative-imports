e772f9770bd3e660c8fb01471113e0b886dcf9e5
Matt Burstein <j.matthew.burstein@gmail.com>
2020-09-05 18:08:21 +0100

    add parser options to tests

--- tests/lib/rules/no-relative-imports.js
+++ tests/lib/rules/no-relative-imports.js
@@ -2,7 +2,12 @@ const RuleTester = require("eslint").RuleTester
 const rule = require("../../lib/rules/no-relative-imports")
 const invalidCodeTest = require("../../utils").invalidCodeTest
 
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2015,
+    sourceType: "module",
+  },
+})
 
 const requireErrorMessage =
   "paths in require statements must be absolute, not relative"
